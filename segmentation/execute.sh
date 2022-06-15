9b3dbbebd92146454607b875b93633bb15ce974b
u-adrian <uuesb@student.kit.edu>
2022-06-15 17:39:17 +0200

    conda activated in script

--- segmentation/execute.sh
+++ segmentation/execute.sh
@@ -4,4 +4,7 @@
 #SBATCH --time=10
 #SBATCH --output=/home/kit/stud/ucxam/dev/ArtXFashion/output.txt
 
+eval "$(conda shell.bash hook)"
+conda activate artXfashion
+
 python3 segmentation/cloth_segmentation.py main --config_path /home/kit/stud/ucxam/dev/ArtXFashion/segmentation/config.json
