6512e812e435c16de8dd103cd0b27b983fc5f55a
u-adrian <uuesb@student.kit.edu>
2022-06-18 13:40:02 +0200

    cleanup

--- segmentation/cloth_segmentation.py
+++ segmentation/cloth_segmentation.py
@@ -17,14 +17,14 @@ class SegmentationModel:
             in_channels=4,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
             classes=1,  # model output channels (number of classes in your dataset)
         ).float().to(self.device)
-        
+
         self.model.load_state_dict(torch.load(self.model_path))
 
     def __create_marker_mask(self, x, y):
         marker_mask = torch.zeros((self.model_image_height,self.model_image_width)).squeeze()
         gauss = transforms.GaussianBlur(kernel_size=5, sigma=1)
 
-        marker_mask[x][y] = 255
+        marker_mask[y][x] = 255
         marker_mask = gauss(marker_mask.unsqueeze(dim=0).type(torch.float32))
 
         return marker_mask.type(torch.uint8).unsqueeze(dim=0)
@@ -35,16 +35,11 @@ class SegmentationModel:
             input_width = image.shape[3]
             input_height = image.shape[2]
 
-            assert (input_height == 512 and input_width == 256)
-            assert (256 > x > 0)
-            assert (512 > y > 0)
+            assert (input_height == self.model_image_height and input_width == self.model_image_width)
+            assert (self.model_image_width > x > 0)
+            assert (self.model_image_height > y > 0)
 
-            image_transforms = transforms.Compose([transforms.Resize(size=(self.model_image_height,self.model_image_width))])
-
-            x_rescaled = x
-            y_rescaled = y
-
-            marker_mask_255_max = self.__create_marker_mask(x_rescaled, y_rescaled)
+            marker_mask_255_max = self.__create_marker_mask(x, y)
 
             image_and_marker = torch.cat((image, marker_mask_255_max), dim=1)
 
